d00c250bd80313a1e57d95ebc876ed205dfc2489
Sharon <omuwaste@gmail.com>
2025-02-19 09:21:23 +0100

    add config changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  basePath: "/hng12_aiTextProcessor",
+  assetPrefix: "/hng12_aiTextProcessor/",
+};
 
 export default nextConfig;
